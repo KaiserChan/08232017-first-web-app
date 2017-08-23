df601ecd1b3b5763a3678086ab5cfae6c799dcb5
KaiserChan <chankaiserkk@gmail.com>
2017-08-23 17:19:28 -0400

    Update: created favorites.erb and updated server with proper route. Completed exercise

--- server.rb
+++ server.rb
@@ -8,10 +8,21 @@ get '/home' do
   erb :index
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/portfolio' do
   erb :gallery
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['sleep', 'eat', 'lift', 'golf', 'read', 'coffee']
   erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = ['http://www.nba.com', 'http://www.nfl.com']
+  erb :favorites
 end
